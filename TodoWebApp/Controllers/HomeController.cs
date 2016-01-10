876db6ac2c350e3c382f052db3cdd0708f7eae57
fuadgit <fuadmail@gmail.com>
2016-01-11 01:39:23 +0600

    contact view change

--- TodoWebApp/Controllers/HomeController.cs
+++ TodoWebApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace TodoWebApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "your contact page";
 
             return View();
         }
