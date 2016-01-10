abb3d7a1b901ab8f3acd380ffae98989d40d4b6a
fuadgit <fuadmail@gmail.com>
2016-01-11 00:37:29 +0600

    initial commit

--- TodoWebApp/Controllers/HomeController.cs
+++ TodoWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TodoWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application is ready";
 
             return View();
         }
